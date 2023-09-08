d906fcedf4103eae7d29542e1f3b8b7b830f0e95
Zelyna Farrell <zsillas@yahoo.com>
2023-09-08 13:43:03 -0700

    Initial Commit

---
+++ ImmersiveModal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImmersiveModal
+//
+//  Created by Zelyna Sillas on 9/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImmersiveModal/ImmersiveModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImmersiveModalApp.swift
+//  ImmersiveModal
+//
+//  Created by Zelyna Sillas on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ImmersiveModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
